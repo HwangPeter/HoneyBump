c50b36073dcf51e517cadd7267037856ce128512
Hwang <mhwang1@tmmc.com>
2019-08-22 22:02:31 -0700

    pulled articles

---
+++ views/aboutUs/aboutus.js
@@ -0,0 +1,58 @@
+(function () {
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyCi8N03o0dLjoU83NoQUES5Vb3bUspOkCI",
+        authDomain: "honeybump-49085.firebaseapp.com",
+        databaseURL: "https://honeybump-49085.firebaseio.com",
+        projectId: "honeybump-49085",
+        storageBucket: "honeybump-49085.appspot.com",
+        messagingSenderId: "800303839442",
+        appId: "1:800303839442:web:545c691401fa9658"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+            updateLogoutButton();
+        }
+    });
+
+    function updateLogoutButton() {
+        document.getElementById("logout").style.backgroundColor = "transparent";
+        document.getElementById("logout").style.color = "#6B686D";
+        document.getElementById("logout").innerText = "LOGOUT";
+        document.getElementById("sideNav-logout").innerText = "LOGOUT";
+        document.getElementById("sideNav-logout").style.backgroundColor = "transparent";
+        document.getElementById("sideNav-logout").style.color = "#6B686D";
+    }
+
+    document.getElementById("hamburger").addEventListener('click', () => {
+        document.getElementById("sideNav").classList.remove("slideOutLeft");
+        document.getElementById("sideNav").style.opacity = "1";
+        document.getElementById("faded-container").style.display = "block";
+    });
+
+    document.getElementById("faded-container").addEventListener('click', () => {
+        document.getElementById("sideNav").classList.add("slideOutLeft");
+        document.getElementById("faded-container").style.display = "none";
+    });
+
+    document.getElementById("logout").addEventListener('click', () => {
+        if (firebase.auth().currentUser) {
+            firebase.auth().signOut();
+            window.location.href = "/";
+        }
+        else {
+            window.location.href = "/signUpLogin";
+        }
+    });
+
+    document.getElementById("sideNav-logout").addEventListener('click', () => {
+        if (firebase.auth().currentUser) {
+            firebase.auth().signOut();
+            window.location.href = "/";
+        }
+        else {
+            window.location.href = "/signUpLogin";
+        }
+    });
+})();
