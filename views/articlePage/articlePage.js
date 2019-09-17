148332a38cfaaf838d32d491e38d7a346692cd8a
Peter Hwang <phwang94@gmail.com>
2019-09-16 20:32:15 -0700

    Changes to footer and header

--- views/articlePage/articlePage.js
+++ views/articlePage/articlePage.js
@@ -9,6 +9,7 @@
         messagingSenderId: "800303839442",
         appId: "1:800303839442:web:545c691401fa9658"
     };
+    includeHTML();
     firebase.initializeApp(firebaseConfig);
     firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
@@ -69,7 +70,9 @@
         document.getElementById("sideNav-logout").style.backgroundColor = "transparent";
         document.getElementById("sideNav-logout").style.color = "#6B686D";
     }
+})();
 
+function addHeaderEventListeners() {
     document.getElementById("hamburger").addEventListener('click', () => {
         document.getElementById("sideNav").classList.remove("slideOutLeft");
         document.getElementById("sideNav").style.opacity = "1";
@@ -100,6 +103,33 @@
             window.location.href = "/signUpLogin";
         }
     });
+}
 
-
-})();
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.insertAdjacentHTML('afterend', this.responseText); }
+                    // if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+    addHeaderEventListeners();
+};
